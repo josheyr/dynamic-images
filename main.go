2d44c3843b4513fac6179837740028c73619a075
josheyr <joshey.raine@outlook.com>
2022-10-14 11:00:49 +0100

    Embed index.html

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	_ "embed"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/josheyr/dynamic-images/generate"
 	"log"
 	"net/http"
+	"os/exec"
 )
+
+//go:embed index.html
+var indexHtml string
 
 func main() {
 	// gorilla mux
@@ -24,8 +30,23 @@ func main() {
 	router.HandleFunc("/single", generate.GenerateSingleImage).Methods("POST")
 	router.HandleFunc("/dynamic", generate.GenerateDynamicImages).Methods("POST")
 	router.HandleFunc("/emptypixel/{info}", generate.HandlePixel).Methods("GET")
-	// serve index.html
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
 
+	// serve index.html
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte(indexHtml))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	go func() {
+		cmd := exec.Command("cmd", "/C", "start", "http://localhost:8080")
+		err := cmd.Run()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	fmt.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
